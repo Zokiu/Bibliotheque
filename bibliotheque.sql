3713b8e4c86ced5385afe6e41d9850b45033bffb
Zokiu <terry.serretta@gmail.com>
2025-06-03 17:18:48 +0200

    modification .sql

--- bibliotheque.sql
+++ bibliotheque.sql
@@ -1,24 +1,27 @@
 CREATE TABLE IF NOT EXISTS livre (
-            id SERIAL PRIMARY KEY,
+            livre_id SERIAL PRIMARY KEY NOT NULL,
             code CHAR(13),
             titre CHAR(38),
-            auteurid CHAR(03),
+            auteur_id SERIAL REFERENCES auteur(auteur_id),
             type CHAR(16),
             annee CHAR(04),
             edition CHAR(23),
+            compt CHAR(03),
+            pret CHAR(03),
             dispo CHAR(03)
            )
            ;
 
 CREATE TABLE IF NOT EXISTS auteur (
-            id SERIAL PRIMARY KEY,
+            auteur_id SERIAL PRIMARY KEY NOT NULL,
             nom CHAR(22),
             prenom CHAR(22)
            )
            ;
 
 CREATE TABLE IF NOT EXISTS emprunt (
-            code CHAR(13),
+            emprunt_id SERIAL PRIMARY KEY NOT NULL,
+            livre_id SERIAL REFERENCES livre(livre_id),
             nom CHAR(22),
             prenom CHAR(22),
             tel CHAR(10),
